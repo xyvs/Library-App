90e09df5ed43ce0a735fa99e0b683195ca132fa6
Jose Francisco Martinez Salgado <admin@jofranmtz.com>
2019-02-06 23:37:47 -0600

    Master Commit

--- library/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-	path('master/', admin.site.urls),
-	path('accounts/', include('django.contrib.auth.urls')),
-	path('', include('index.urls'))
-]
